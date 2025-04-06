88fc31fc60b605f07f01cf0962e186f0137f48c6
Pascal-Juma <pascaljuma2002@gmail.com>
2025-04-06 17:02:33 +0300

    Build the Updating function of the products api

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client'
+import validateProduct from './middlewares/validateProduct.js';
 
 const client = new PrismaClient();
 const app = express();
@@ -8,8 +9,7 @@ app.use(express.json());
 app.get("/products", (_req, res) =>{
     res.send("Getting all products")
 })
-app.post("/products", async (req, res) => {
-    console.log(req.body);
+app.post("/products", validateProduct, async (req, res) => {
     const { productTitle, productDescription, unitsLeft, pricePerUnit} = req.body;
     try{
         const newProduct = await client.productItem.create({
@@ -20,10 +20,43 @@ app.post("/products", async (req, res) => {
                 pricePerUnit
             }
         })
-        console.log(newProduct)
+        res.status(201).json({
+            status: "Success",
+            message: "New Product added successfully",
+            data: newProduct
+        })
     }catch (e){
-        console.log(e)
         res.json({
+            message: "Something went wrong. Try again later"
+        })
+    }
+})
+
+app.patch("/products/:productId", async (req, res) => {
+    const { productTitle, productDescription, unitsLeft, pricePerUnit, isOnOffer } = req.body;
+    const { productId } = req.params;
+    try{
+        const updatedItem = await client.productItem.update({
+            where: {
+                id: productId
+            },
+            data: {
+                productTitle: productTitle && productTitle,
+                productDescription: productDescription && productDescription,
+                unitsLeft: unitsLeft && unitsLeft,
+                pricePerUnit: pricePerUnit && pricePerUnit,
+                isOnOffer: isOnOffer && isOnOffer
+            }
+        })
+        res.status(200).json({
+            status: "Success",
+            message: "Product updated Successfully",
+            data: updatedItem
+        })
+    }catch(e){
+        console.log(e)
+        res.status(500).json({
+            status: "Error",
             message: "Something went wrong. Try again later"
         })
     }
---
+++ middlewares/validateProduct.js
@@ -0,0 +1,30 @@
+function validateProduct(req, res, next){
+    const { productTitle, productDescription, unitsLeft, pricePerUnit} = req.body;
+    if(!productTitle){
+        res.status(400).json({
+            status: "Error",
+            message: "Product title is required"
+        })
+    }
+    if(!productDescription){
+        res.status(400).json({
+            status: "Error",
+            message: "Product Description is required"
+        })
+    }
+    if(!unitsLeft){
+        res.status(400).json({
+            status: "Error",
+            message: "Units Left is Required"
+        })
+    }
+    if(!pricePerUnit){
+        res.status(400).json({
+            status: "Error",
+            message: "Price Per Unit of the Product is required"
+        })
+    }
+    next();
+}
+
+export default validateProduct;
