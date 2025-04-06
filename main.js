b494a5cc86eb439900bab7441f5ec380e35ef145
Pascal-Juma <pascaljuma2002@gmail.com>
2025-04-06 18:51:57 +0300

    Update the Products API to include the functionality of getting all products on offer

--- main.js
+++ main.js
@@ -121,4 +121,32 @@ app.delete("/products/:productId", async (req, res) => {
     }
 })
 
+app.get("/productsOnOffer", async (_req, res) => {
+    try{
+        const productsOnOffer = await client.productItem.findMany({
+            where: {
+                isOnOffer: true
+            }
+        });
+        if(productsOnOffer == false){
+            res.status(404).json({
+                status: "Error",
+                message: "Products On Offer not found"
+            })
+        }else{
+        res.status(200).json({
+            status: "Success",
+            message: "Product(s) on offer",
+            data: productsOnOffer
+        })
+    }
+    }catch(e){
+        console.log(e)
+        res.status(500).json({
+            status: "Error",
+            message: "Something went wrong. Try again later"
+        })
+    }
+})
+
 export default app;
